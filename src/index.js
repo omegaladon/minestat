98e06fcc0464daeb5713a2735602ba22f4c0a708
omegaladon <businessomga@gmail.com>
2023-06-09 10:41:40 -0700

    include express server to ping

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const Bot = require("./bot");
 require("dotenv").config();
 const TOKEN = process.env.TOKEN;
 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("👋");
+});
+
 const bot = new Bot(TOKEN);
 bot.registerCommands();
 bot.registerEvents();
